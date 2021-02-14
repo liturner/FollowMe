7ff2d6a7a2430d9372c31d594320543eb25340f6
Luke Turner <luke.ian.turner@outlook.de>
2021-02-14 16:13:36 +0100

    add unit popup menu

--- FollowMe.lua
+++ FollowMe.lua
@@ -10,6 +10,7 @@ FollowMe.StringTableDE.Title = "Folge Mir!"
 FollowMe.StringTableDE.Description = "Dieses Addon ermöglicht das '/folgemir' Kommando. Es wird verwendet, um einem anderen Charakter zu sagen, dass er dir folgen soll. Der Befehl eignet sich perfekt für den Fall, dass ein Mitglied deines Teams für einige Minuten im echten Leben weg muss. Solange das Teammitglied und du dieses Addon besitzen, kannst du die Wartezeit verhindern oder auch, dass du deinen Freund zurücklassen musst.\n\nSelbstverständlich funktioniert das Kommando nur in aktiven Gruppen, sodass du nicht von Fremden genervt werden kannst."
 FollowMe.StringTableDE.ControlsSubTitle = "Steuerung"
 FollowMe.StringTableDE.ControlsText = "Unabhängig von der Sprache, die du verwendest, kann jedes der folgenden Kommandos einen anderen 'Folge Mir!' Benutzer instruieren, dir zu folgen:\n\n/folgemir [SPIELER_NAME]\n/followme [SPIELER_NAME]"
+FollowMe.StringTableDE.UnitPopup =  "Folge Mir"
 
 FollowMe.StringTableEN = {}
 FollowMe.StringTableDE.Command = "/followme"
@@ -17,9 +18,7 @@ FollowMe.StringTableEN.Title = "Follow Me!"
 FollowMe.StringTableEN.Description = "This Addon implements the '/followme' command. This command is used to tell a charachter they must follow you. Perfect for the case you realise that a team member is away for a few minutes because his IRL baby is crying... As long as the team member and you have this addon, you can prevent either waiting ten minutes, or leaving your friend behind.\n\nThe command of course only works within active groups, so you cannot just be terrorised by randomers!"
 FollowMe.StringTableEN.ControlsSubTitle = "Controls"
 FollowMe.StringTableEN.ControlsText = "Regardless of your clients interface language, any of the following commands can be used to instruct another 'Follow Me!' user to follow you:\n\n/followme [PLAYER_NAME]\n/folgemir [PLAYER_NAME]"
-
-
-
+FollowMe.StringTableDE.UnitPopup =  "Follow Me"
 
 function FollowMe.OnLoad(self)
 	self:RegisterEvent('ADDON_LOADED')
@@ -66,9 +65,32 @@ function FollowMe.OnAddonLoaded(self, name, ...)
 			FollowMe.SlashCommand(self, msg, editbox)
 		end
 		
-		 C_ChatInfo.RegisterAddonMessagePrefix("TTFM")
+		C_ChatInfo.RegisterAddonMessagePrefix("TTFM")
 		
 		self:UnregisterEvent("ADDON_LOADED")
+		
+		hooksecurefunc("UnitPopup_ShowMenu", function()
+			if (UIDROPDOWNMENU_MENU_LEVEL > 1) then
+				return
+			end
+			
+			-- The selected chars name will be in here
+			local dropdownMenu = _G["UIDROPDOWNMENU_INIT_MENU"]
+			
+			-- Only charachters in group can utilise this addon, so dont hook the option otherwise
+			if not IsInGroup(dropdownMenu.name) then
+				return
+			end
+
+			local info = UIDropDownMenu_CreateInfo()
+			info.text = FollowMe.i13n.UnitPopup
+			info.owner = which
+			info.notCheckable = 1
+			info.func = FollowMe.FollowMeButton
+			info.value = "FollowMeButton"
+
+			UIDropDownMenu_AddButton(info)
+		end)
 	end	
 end
 
@@ -76,6 +98,15 @@ function FollowMe.SlashCommand(self, msg, editbox)
 	C_ChatInfo.SendAddonMessage("TTFM", FollowMe.Globals.PLAYER_NAME, "WHISPER", msg)
 end
 
+function FollowMe.FollowMeButton(self)
+	if self.value == "FollowMeButton" then
+		local dropdownMenu = _G["UIDROPDOWNMENU_INIT_MENU"]
+
+		if(dropdownMenu.name ~= UnitName("player")) then
+			C_ChatInfo.SendAddonMessage("TTFM", FollowMe.Globals.PLAYER_NAME, "WHISPER", dropdownMenu.name)
+		end
+	end
+end
 
 -- Options UI Functions
 function FollowMe.OK_Clicked(self)
